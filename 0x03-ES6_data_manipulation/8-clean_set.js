dfb00b77f12945a8054af328f4f3fd0701d412ff
charles ezebuike <ezebuike.charles0@gmail.com>
2023-07-04 18:46:07 +0000

    task 8:function returns a string of all the set values that start with a specific string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  let res = '';
+  if (!(startString && typeof startString === 'string')) {
+    return res;
+  }
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      res += `${value.substring(startString.length)}-`;
+    }
+  }
+  return res.slice(0, -1);
+}
