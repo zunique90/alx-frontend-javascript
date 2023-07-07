419d5052bd42a28edc941c040f3ddc0f8baf9af1
charles ezebuike <ezebuike.charles0@gmail.com>
2023-07-07 20:49:19 +0000

    task 9 & 10: Namespace & Declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,20 @@
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for React";
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher.experienceTeachingReact) {
+        return "No available teacher";
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
